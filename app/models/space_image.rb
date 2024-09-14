048f9f9881227f3372bdb4743234654ce397d2ee
Chris Miles <cmiles@obm.com>
2024-09-14 13:54:27 -0400

    validations

--- app/models/space_image.rb
+++ app/models/space_image.rb
@@ -1,2 +1,8 @@
 class SpaceImage < ApplicationRecord
+  validates :description, presence: true
+  validates :guid, presence: true, uniqueness: true
+  validates :image_url, presence: true
+  validates :link, presence: true
+  validates :publication_date, presence: true
+  validates :title, presence: true
 end
