9280c100b2521d50292ab05bd83363c9a7e9ce99
Chris Miles <cmiles@obm.com>
2024-09-15 13:49:22 -0400

    keyframes for spinner

--- app/javascript/components/SpaceImage/index.js
+++ app/javascript/components/SpaceImage/index.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Spinner from '../Spinner'
 
 export default function SpaceImage({ image, onClick }) {
+  const [loading, setLoading] = useState(true)
+
+  const handleImageLoad = () => {
+    setLoading(false)
+  }
+
   const styles = {
     imageWrapper: {
       cursor: 'pointer',
@@ -15,12 +22,29 @@ export default function SpaceImage({ image, onClick }) {
       width: '100%',
       height: '100%',
       objectFit: 'cover',
+      display: loading ? 'none' : 'block',
+    },
+    spinnerWrapper: {
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
     },
   }
 
   return (
     <div onClick={onClick} style={styles.imageWrapper}>
-      <img src={image.image_url} alt={image.title} style={styles.image} />
+      {loading && (
+        <div style={styles.spinnerWrapper}>
+          <Spinner size={40} /> {/* Using smaller spinner */}
+        </div>
+      )}
+      <img
+        src={image.image_url}
+        alt={image.title}
+        style={styles.image}
+        onLoad={handleImageLoad}
+      />
     </div>
   )
 }
--- app/javascript/components/Spinner/index.js
+++ app/javascript/components/Spinner/index.js
@@ -1,20 +1,12 @@
 import React from 'react'
 
-export default function Spinner() {
+export default function Spinner({ size = 40 }) {
   const styles = {
     spinner: {
-      border: '16px solid #f3f3f3',
-      borderTop: '16px solid #1D1D21', 
-      borderRadius: '50%',
-      width: '120px',
-      height: '120px',
-      animation: 'spin 2s linear infinite',
-    },
-    '@keyframes spin': {
-      '0%': { transform: 'rotate(0deg)' },
-      '100%': { transform: 'rotate(360deg)' },
+      width: `${size}px`,
+      height: `${size}px`,
     },
   }
 
-  return <div style={styles.spinner}></div>
+  return <div className="spinner" style={styles.spinner} />
 }
